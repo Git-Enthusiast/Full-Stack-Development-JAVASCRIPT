02bbae094d89499cb5b1e7c76b6e5745de83a22b
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2023-12-25 16:18:45 +0530

    JavaScript Lecture 7

--- Lec_7/p1/script.js
+++ Lec_7/p1/script.js
@@ -8,6 +8,36 @@ const newwords = words;
 newwords[1]="Kiwi";
 console.log(words);
 console.log(newwords);
+
+
+// here "student" is an non-primitive data types called objects
+// object is a  collection of differernt data types . 
+
+const student ={
+   fullName : "Rajan Raj",
+   age : 21,
+   cgpa : 8.8,
+   isPass : true,
+   // key: value,
+};
+console.log(student);
+console.log(student["fullName"]);
+console.log(student.fullName);
+console.log(student["age"]);
+// both is valid format
+// to change value of object :
+student["fullName"]= "Om Pradhan";
+// or 
+student.fullName = "Om Pradhan";
+student.cgpa = 9.1;
+// we can not change value of a const variable but
+// we can change key of a const object  
+// here student is an object but we are able to change its key
+// i.e. name, cgpa etc.
+console.log(typeof(student));
+console.log(typeof(student["age"]));
+
+console.log(student);
 //  index()  --> searches an element of an array and returns its postion.
 // includes()  --> check if an array contains an specific element or not
 // push()  --> add a new element to the end of an array and returns new length of an array.
